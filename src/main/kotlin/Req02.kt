6bb02eab69c6205c3792f57b0af21e61d559d403
Luanderson Alves <luaoderson@gmail.com>
2023-10-24 19:17:12 -0300

    feat: req02 - functions to calculate commission and final salary

--- src/main/kotlin/Req02.kt
+++ src/main/kotlin/Req02.kt
@@ -1,10 +1,6 @@
-fun calculateCommission(salesAmount: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateCommission(salesAmount: Double): Double = salesAmount * 0.04
 
-fun calculateFinalSalary(baseSalary: Double, commission: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateFinalSalary(baseSalary: Double, commission: Double): Double = baseSalary + commission
 
 fun main() {
     TODO("Use essa função para rodar o codigo principal")
