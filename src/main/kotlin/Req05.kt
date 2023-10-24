01d34055ee7e40f48fe0f6547f5d08f3e5b695bb
Luanderson Alves <luaoderson@gmail.com>
2023-10-24 20:06:35 -0300

    feat: req05 - function to calculate tax

--- src/main/kotlin/Req05.kt
+++ src/main/kotlin/Req05.kt
@@ -1,5 +1,15 @@
+fun applyTax(price: Double, tax: Double): Double = price * tax
+
 fun calculateConsumerPrice(factoryCost: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+
+    val finalPrice: Double = if (factoryCost <= 12000.0) {
+        factoryCost + applyTax(factoryCost, 0.05)
+    } else if (factoryCost > 12000.0 && factoryCost <= 25000.0) {
+        factoryCost + applyTax(factoryCost, 0.10) + applyTax(factoryCost, 0.15)
+    } else {
+        factoryCost + applyTax(factoryCost, 0.15) + applyTax(factoryCost, 0.20)
+    }
+    return finalPrice
 }
 
 fun main() {
