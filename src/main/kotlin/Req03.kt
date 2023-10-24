1d7f7167563fdcc945e1f9a4b78e15497b8bc554
Luanderson Alves <luaoderson@gmail.com>
2023-10-24 19:42:57 -0300

    feat: req03 - functions to calculate years, months, days and weeks

--- src/main/kotlin/Req03.kt
+++ src/main/kotlin/Req03.kt
@@ -1,18 +1,11 @@
-fun calculateYears(birthYear: Int, currentYear: Int): Int {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateYears(birthYear: Int, currentYear: Int): Int = currentYear - birthYear
 
-fun calculateMonths(birthYear: Int, currentYear: Int): Int {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateMonths(birthYear: Int, currentYear: Int): Int = calculateYears(birthYear, currentYear) * 12
 
-fun calculateDays(birthYear: Int, currentYear: Int): Long {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateDays(birthYear: Int, currentYear: Int): Long =
+    (((currentYear - birthYear) / 4) + ((currentYear - birthYear) * 365)).toLong()
 
-fun calculateWeeks(birthYear: Int, currentYear: Int): Long {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateWeeks(birthYear: Int, currentYear: Int): Long = calculateDays(birthYear, currentYear) / 7
 
 fun main() {
     TODO("Use essa função para rodar o codigo principal")
