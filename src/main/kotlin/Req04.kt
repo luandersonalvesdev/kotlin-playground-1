faffaa203e29894647bff8b169ac4ed932ddee0e
Luanderson Alves <luaoderson@gmail.com>
2023-10-24 19:59:02 -0300

    feat: req04 - functions to calculate some numbers

--- src/main/kotlin/Req04.kt
+++ src/main/kotlin/Req04.kt
@@ -1,17 +1,16 @@
-fun calculateAverage(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateAverage(number1: Double, number2: Double): Double = (number1 + number2) / 2
 
 fun calculateDifference(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    val bigger = if (number1 > number2) number1 else number2
+    val smaller = if (number1 < number2) number1 else number2
+    return bigger - smaller
 }
 
-fun calculateProduct(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateProduct(number1: Double, number2: Double): Double = number1 * number2
 
 fun calculateDivision(number1: Double, number2: Double): Double? {
-    TODO("Use essa função para desenvolver sua lógica")
+    if (number2 == 0.0) return null
+    return number1 / number2
 }
 
 fun main() {
