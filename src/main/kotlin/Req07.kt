e2ecb17f965b13b3c5c3feb485bb483924cb7876
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 18:46:44 -0300

    feat: req07 - function to calculate some infos

--- src/main/kotlin/Req07.kt
+++ src/main/kotlin/Req07.kt
@@ -1,13 +1,27 @@
-fun calculateAverage(ages: List<Int>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateAverage(ages: List<Int>): Double = ((ages.reduce{acc, age -> acc + age}) / ages.size).toDouble()
 
 fun countPeopleWeightHeight(weights: List<Double>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    var total = 0
+
+    for (i in 0 until weights.size) {
+        if (weights[i] > 90.0 && heights[i] < 1.50) {
+            total += 1
+        }
+    }
+
+    return total
 }
 
 fun calculatePercentageAgeHeight(ages: List<Int>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    var total = 0
+
+    for (i in 0 until ages.size) {
+        if (ages[i] in 10..30 && heights[i] > 1.90) {
+            total += 1
+        }
+    }
+
+    return ((total * 100) / ages.size)
 }
 
 fun main() {
