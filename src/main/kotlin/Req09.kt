48259ea0576e8cf95a40613226a08bc541db7fca
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 19:32:40 -0300

    feat: req09 - function to calculate final price of car

--- src/main/kotlin/Req09.kt
+++ src/main/kotlin/Req09.kt
@@ -1,5 +1,13 @@
 fun calculateFinalPrice(carValue: Double, installment: Int, surcharges: Array<Double>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    val percent = when (installment) {
+        12 -> surcharges[0]
+        24 -> surcharges[1]
+        36 -> surcharges[2]
+        48 -> surcharges[3]
+        60 -> surcharges[4]
+        else -> 0.0
+    }
+    return if (percent != 0.0) (carValue + (carValue * percent)) else (carValue - (carValue * 0.20))
 }
 
 fun main() {
