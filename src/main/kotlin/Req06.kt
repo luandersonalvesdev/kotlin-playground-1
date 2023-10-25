fba71fdc51436387853e0943951d53ad2c315e8e
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 18:12:05 -0300

    feat: req06 - function to calculate salary increase

--- src/main/kotlin/Req06.kt
+++ src/main/kotlin/Req06.kt
@@ -1,5 +1,21 @@
+import applyTax
+
 fun calculateNewSalary(currentSalary: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    val newSalary: Double = if (currentSalary <= 300.0) {
+        currentSalary + applyTax(currentSalary, 0.50)
+    } else if (currentSalary > 1000.0) {
+        currentSalary + applyTax(currentSalary, 0.05)
+    } else if (currentSalary > 300.0 && currentSalary <= 500.0) {
+        currentSalary + applyTax(currentSalary, 0.40)
+    } else if (currentSalary > 500.0 && currentSalary <= 700.0) {
+        currentSalary + applyTax(currentSalary, 0.30)
+    } else if (currentSalary > 700.00 && currentSalary <= 800.0) {
+        currentSalary + applyTax(currentSalary, 0.20)
+    } else {
+        currentSalary + applyTax(currentSalary, 0.10)
+    }
+
+    return newSalary
 }
 
 fun main() {
