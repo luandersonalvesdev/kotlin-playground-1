a1c15563db51d1a0a66a07c553cb85c6159b9e85
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 20:14:42 -0300

    feat: req11 - function to count negative numbers and sum positives

--- src/main/kotlin/Req11.kt
+++ src/main/kotlin/Req11.kt
@@ -1,10 +1,7 @@
-fun countNegativeNumbers(numbers: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun countNegativeNumbers(numbers: List<Double>): Int = numbers.filter { it < 0 }.size
 
-fun calculateSumOfPositiveNumbers(numbers: List<Double>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateSumOfPositiveNumbers(numbers: List<Double>): Double =
+    numbers.filter { it > 0 }.reduce { acc, it -> acc + it }
 
 fun main() {
     TODO("Use essa função para rodar o codigo principal")
