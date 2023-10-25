a50dd7f197bf6d7667e81821b94ab1735c01684e
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 19:53:56 -0300

    feat: req10 - function to find multiples of 2, 3 and 2 and 3

--- src/main/kotlin/Req10.kt
+++ src/main/kotlin/Req10.kt
@@ -1,14 +1,8 @@
-fun findMultiplesOf2(numbers: IntArray): List<Int> {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun findMultiplesOf2(numbers: IntArray): List<Int> = numbers.filter { it % 2 == 0 }
 
-fun findMultiplesOf3(numbers: IntArray): List<Int> {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun findMultiplesOf3(numbers: IntArray): List<Int> = numbers.filter { it % 3 == 0 }
 
-fun findMultiplesOf2And3(numbers: IntArray): List<Int> {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun findMultiplesOf2And3(numbers: IntArray): List<Int> = numbers.filter { it % 2 == 0 && it % 3 == 0 }
 
 fun main() {
     TODO("Use essa função para rodar o codigo principal")
