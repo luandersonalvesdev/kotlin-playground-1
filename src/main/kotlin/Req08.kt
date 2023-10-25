a8d462b2262b1cb31fdfa9978c4f62303547f515
Luanderson Alves <luaoderson@gmail.com>
2023-10-25 19:09:19 -0300

    feat: req08 - function to sum odd and even numbers

--- src/main/kotlin/Req08.kt
+++ src/main/kotlin/Req08.kt
@@ -1,10 +1,6 @@
-fun calculateSumEven(numbers: List<Int>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateSumEven(numbers: List<Int>): Int = numbers.filter { it % 2 == 0 }.reduce { acc, it -> acc + it }
 
-fun calculateSumOdd(numbers: List<Int>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
-}
+fun calculateSumOdd(numbers: List<Int>): Int = numbers.filter { it % 2 != 0 }.reduce { acc, it -> acc + it }
 
 fun main() {
     TODO("Use essa função para rodar o codigo principal")
